69c60673e01d4b16fb219ada0b2d41190fd7918e
Yaroslav82 <yargay230607@gmail.com>
2023-07-08 13:53:34 +0300

    Add mode constants

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,6 +1,9 @@
 package utils;
 
 public class Constants {
-
     public final static String BASE_PATH_IN = "files/";
+
+    public final static String WRITING_MODE = "w";
+    public final static String READING_MODE = "r";
+    public final static String UNKNOWN_MODE = "Unknown mode :(";
 }
