f32377749c229c1888e1dbda8042570d31a3398b
Yaroslav82 <yargay230607@gmail.com>
2023-07-08 13:11:00 +0300

    Create file writer

---
+++ src/app/FileWriteService.java
@@ -0,0 +1,18 @@
+package app;
+
+import utils.Constants;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileWriteService {
+    public String handleFile(String myText, String fileName) {
+        try (FileOutputStream fout = new FileOutputStream(Constants.BASE_PATH_IN + fileName)) {
+            byte[] byteArr = myText.getBytes();
+            fout.write(byteArr);
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+        return "Success!";
+    }
+}
