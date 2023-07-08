d6b1334d0a9e0495b99df67964ca1cca660133ab
Yaroslav82 <yargay230607@gmail.com>
2023-07-08 13:23:38 +0300

    Create file reader

---
+++ src/app/FileReadService.java
@@ -0,0 +1,24 @@
+package app;
+
+import utils.Constants;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileReadService {
+
+    public String readFile(String fileName) {
+        try {
+            FileInputStream fin = new FileInputStream(Constants.BASE_PATH_IN + fileName);
+            StringBuilder builder = new StringBuilder();
+            int i;
+            while ((i = fin.read()) != -1) {
+                builder.append((char) i);
+            }
+            return builder.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
--- src/app/FileWriteService.java
+++ src/app/FileWriteService.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileWriteService {
+
     public String handleFile(String myText, String fileName) {
         try (FileOutputStream fout = new FileOutputStream(Constants.BASE_PATH_IN + fileName)) {
             byte[] byteArr = myText.getBytes();
